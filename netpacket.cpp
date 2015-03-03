d160064270aed54e5dee7ef8e3919fd21cf4269b
unknown <Marcusyang@JY000411.intranet.123u.com>
2015-03-03 16:07:10 +0800

    change netpacket.h/cpp

--- netpacket.cpp
+++ netpacket.cpp
@@ -16,27 +16,28 @@ bool NETPacket::GetData(int clientFd, const char* pDataBuffer, const unsigned in
     {
     	return false;
     }
-    char *p = pDataBuffer;  
-    int len = nDataSize;  
-    int ret = 0;  
-    int returnlen = 0;
-      
-    while( len > 0)  
-    {   
-    	ret = recv(clientFd, p+(iLen-len), iLen-returnlen, 0 );   
-    	if ( ret == SOCKET_ERROR || ret == 0 )   
-    	{        
-    		return false;   
-    	}   
-    	len -= ret;   
-    	returnlen += ret;  
-    }    
-    return returnlen;
+
+    char *p = pDataBuffer;
+    int len = nDataSize;
+    int ret = 0;
+    int returnlen = 0;
+
+    while (len > 0)
+    {
+    	ret = recv(clientFd, p + (iLen - len), iLen - returnlen, 0);
+    	if (ret ==  SOCKET_ERROR || ret == 0)
+    	{
+    		return false;
+    	}
+    	len -= ret;
+    	returnlen += ret;
+    }
+    return returnlen;
 }  
   
 bool NETPacket::SendData( int clientFd, const char* pDataBuffer, const unsigned int& nDataSize )  
 {  
-    NetPacketHeader* pHead = (NetPacketHeader*) m_cbSendBuf;  
+   /* NetPacketHeader* pHead = (NetPacketHeader*) m_cbSendBuf;  
     pHead->wOpcode = nOpcode;//操作码  
   
     // 数据封包  
@@ -49,5 +50,6 @@ bool NETPacket::SendData( int clientFd, const char* pDataBuffer, const unsigned 
     const unsigned short nSendSize = nDataSize + sizeof(NetPacketHeader);//包的大小事发送数据的大小加上包头大小  
     pHead->wDataSize = nSendSize;//包大小  
     int ret = ::send(mAcceptSocket, m_cbSendBuf, nSendSize, 0);  
-    return (ret > 0) ? true : false;  
+    return (ret > 0) ? true : false;  */
+    return false;
 }  
