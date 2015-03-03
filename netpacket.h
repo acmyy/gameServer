1f78402b047075440a9bd3dda220029963dcffec
unknown <Marcusyang@JY000411.intranet.123u.com>
2015-03-03 16:01:08 +0800

    change netpacket.h

--- netpacket.h
+++ netpacket.h
@@ -14,12 +14,12 @@ public:
     /// 发送数据  
     bool SendData(int clientFd, const char* pDataBuffer, const unsigned int& nDataSize);  
   
-private:  
+/*private:  
     SOCKET      mServerSocket;  ///< 服务器套接字句柄  
     sockaddr_in mServerAddr;    ///< 服务器地址  
   
     SOCKET      mAcceptSocket;  ///< 接受的客户端套接字句柄  
     sockaddr_in mAcceptAddr;    ///< 接收的客户端地址  
   
-    char        m_cbSendBuf[NET_PACKET_SIZE];  
+    char        m_cbSendBuf[NET_PACKET_SIZE];  */
 };  
